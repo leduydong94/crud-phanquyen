b139c99726de1ffa0be5772a02ad07e8f698fb2b
leduydong94 <53608934+leduydong94@users.noreply.github.com>
2020-04-30 17:16:12 +0700

    Update readDataAdmin.php

--- readDataAdmin.php
+++ readDataAdmin.php
@@ -33,7 +33,7 @@
  								<span class="icon-bar"></span>
  								<span class="icon-bar"></span>
  							</button>
- 							<a class="navbar-brand" href="readData.php">User Information</a>
+ 							<a class="navbar-brand" href="readDataAdmin.php">User Information</a>
  						</div>
  				
  						<!-- Collect the nav links, forms, and other content for toggling -->
