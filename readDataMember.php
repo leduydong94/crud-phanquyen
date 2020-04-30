8197a23498b08882f1a5857947e0b1d78e6bdbaa
leduydong94 <53608934+leduydong94@users.noreply.github.com>
2020-04-30 17:16:55 +0700

    Update readDataMember.php

--- readDataMember.php
+++ readDataMember.php
@@ -31,7 +31,7 @@
  								<span class="icon-bar"></span>
  								<span class="icon-bar"></span>
  							</button>
- 							<a class="navbar-brand" href="readData.php">User Information</a>
+ 							<a class="navbar-brand" href="readDataMember.php">User Information</a>
  						</div>
  				
  						<!-- Collect the nav links, forms, and other content for toggling -->
